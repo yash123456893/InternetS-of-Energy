679d26dd94e2508858d1ca7875658ac9ec55b994
yash123456893 <56687394+yash123456893@users.noreply.github.com>
2021-04-20 18:18:34 +0530

    Update bottle.py

--- bottle.py
+++ bottle.py
@@ -7,11 +7,7 @@ a built-in HTTP Server and adapters for many third party WSGI/HTTP-server and
 template engines - all in a single file and with no dependencies other than the
 Python Standard Library.
 
-Homepage and documentation: http://bottlepy.org/
 
-Copyright (c) 2014, Marcel Hellkamp.
-License: MIT (see LICENSE for details)
-"""
 
 from __future__ import with_statement
 
